e1978ef79f041dda4db9cdded9dc7c2968947fc7
Aanchal <aanchal.singh@thoughtworks.com>
2020-02-07 23:42:02 +0530

    [Aanchal] Add. Menu to display option

--- src/main/java/com/twu/biblioteca/Bibliotecha.java
+++ src/main/java/com/twu/biblioteca/Bibliotecha.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Bibliotecha {
 
@@ -15,15 +16,30 @@ public class Bibliotecha {
         Library library = new Library();
         List<Book> books = library.displayBooks();
 
-        System.out.println("Name"+"         |           "+"Author"+"            |           "+"Publisher");
+        System.out.println("Name" + "         |           " + "Author" + "            |           " + "Publisher");
         for (Book book : books) {
-            System.out.println(book.getName() + "           |           " + book.getAuthor() +"         |           "+ book.getPublicationYear());
+            System.out.println(book.getName() + "           |           " + book.getAuthor() + "         |           " + book.getPublicationYear());
         }
+    }
+
+    void menu() {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+
+        System.out.println("Biblioteca");
+        System.out.println("a) List of books");
+        System.out.println("Enter choice : ");
+        choice = scanner.next();
+
+        if ("a".equals(choice)) {
+            DisplayAllBooks();
+        }
+
     }
 
     public static void main(String[] args) {
         Bibliotecha bibliotecha = new Bibliotecha();
         bibliotecha.welcomeMessage();
-        bibliotecha.DisplayAllBooks();
+        bibliotecha.menu();
     }
 }
