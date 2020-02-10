dc90e9354976ea0677ec143e3281ce5e0775ef16
Aanchal <aanchal.singh@thoughtworks.com>
2020-02-10 19:04:19 +0530

    Refractor. Removed redundant test cases

--- src/test/java/com/twu/biblioteca/MenuTest.java
+++ src/test/java/com/twu/biblioteca/MenuTest.java
@@ -9,48 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
     @Test
-    public void shouldAddOneMenuItemToMenu() {
-        MenuItem menuItem = new ViewBooks();
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(menuItem);
-
-        Menu menu = new Menu(menuItems);
-
-        assertEquals(1, menu.itemSize());
-    }
-
-    @Test
-    public void shouldAddTwoMenuItemToMenu() {
-        MenuItem menuItem1 = new ViewBooks();
-        MenuItem menuItem2 = new checkOut();
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(menuItem1);
-        menuItems.add(menuItem2);
-
-        Menu menu = new Menu(menuItems);
-
-        assertEquals(2, menu.itemSize());
-    }
-
-
-    @Test
-    public void shouldAddThreeMenuItemToMenu() {
-        MenuItem menuItem1 = new ViewBooks();
-        MenuItem menuItem2 = new checkOut();
-        MenuItem menuItem3 = new returnBook();
-
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(menuItem1);
-        menuItems.add(menuItem2);
-        menuItems.add(menuItem3);
-
-        Menu menu = new Menu(menuItems);
-
-        assertEquals(3, menu.itemSize());
-    }
-
-
-    @Test
     public void shouldAddAllMenuItemToMenu() {
         MenuItem menuItem1 = new ViewBooks();
         MenuItem menuItem2 = new checkOut();
