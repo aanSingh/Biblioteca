9bf7cea7116199fa96f88d9feaf374d0eca6d6f0
Aanchal <aanchal.singh@thoughtworks.com>
2020-02-12 22:24:20 +0530

    [Aanchal] Add. Test case for returning movie unsuccessfully

--- src/test/java/com/twu/biblioteca/logic/LibraryTest.java
+++ src/test/java/com/twu/biblioteca/logic/LibraryTest.java
@@ -189,4 +189,22 @@ class LibraryTest {
         assertDoesNotThrow(() -> library.returnMovie(movieName));
     }
 
+    @Test
+    public void shouldThrowExceptionWhenMovieReturnedSuccessfully() throws InvalidMovieException {
+        Movie movie1 = new Movie("A Nightmare on Elm Street", 1984, "Wes Craven", 8);
+        Movie movie2 = new Movie("The Shawshank Redemption", 1994, " Frank Darabont", 9);
+        Movie movie3 = new Movie("The Godfather", 1972, "Francis Ford Coppola", 9);
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+        movies.add(movie3);
+        Library library = new Library(null, movies);
+        String movieCheckout = "A Nightmare on Elm Street";
+
+        library.checkoutMovie(movieCheckout);
+
+        String movieReturn = "A Nightmare on Elm";
+        assertThrows(InvalidMovieException.class, () -> library.returnMovie(movieReturn));
+    }
+
 }
